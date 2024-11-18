41ee1c31af60e0d6f73e91dea10caadab2c796d9
Filip <flp@Filips-MacBook-Pro.local>
2024-11-18 07:09:52 +0100

    add presets.js

---
+++ js/presets.js
@@ -0,0 +1,12 @@
+// `presets.js`
+
+const Presets = {
+    assetsBaseUrl: 'https://assets.lttrcorp.com/',
+    jsonUrl: 'https://assets.lttrcorp.com/2024-10-01-ai-font-tester/ai_testing_samples/fonts_data.json',
+    defaultPresets: {
+        model: 'LTTR24-FT',
+        epochs: 1100,
+        samples: 30,
+        fontNumber: '0006'
+    }
+};
